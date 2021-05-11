c77d1fd5d0c7b2077e3b8f847f0baad6f61d4302
o2caledonie <krn.sigura@yahoo.com>
2021-05-11 08:59:35 +0200

    product.js - page Teddy

--- front/js/product.js
+++ front/js/product.js
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id');
 console.log(id);
 
-function fetchTeddies() {
+function fetchTeddy() {
     fetch("http://localhost:3000/api/teddies/" + id)
     .then(res => {
         console.log(res)
@@ -14,15 +14,24 @@ function fetchTeddies() {
         return res.json();
     })
 
-    .then(teddies => {
-        console.log(teddies);
-        console.log(teddies.colors);
+    .then(teddy => {
+        console.log(teddy);
         let newElt = buildTeddyPage(teddy);
         console.log(buildTeddyPage(teddy));
         let elt = document.getElementById('teddy');
-        elt.appendChild(newElt);
+        elt.appendChild(newElt);  
+        
+        const colors = teddy.colors;
+        console.log(colors)
+        for (let i of colors) {
+            let color = customTeddy(i);
+            console.log(customTeddy(i));
+            let opt = document.getElementsByClassName('custom-select');
+            opt.appendChild(color);
+        }
     })
 
+    
     .catch(error => {
         console.log(error);
         document.getElementById('erreur').innerHTML = "Erreur lors de la récupération des données :("
@@ -33,22 +42,33 @@ function fetchTeddies() {
 function buildTeddyPage(teddy) {
     const newElt = document.createElement("div");
     newElt.classList.add("card");
-    newElt.innerHTML = `<img class="card-img-top" src="${teddy.imageUrl}" alt="Card image">
-    <div class="card-body">
-      <h4 class="card-title">Norbert</h4>
-      <p class="card-text">Description</p>
-      <div>Prix: <span class="price">29 euros</span></div>
-      <form>Choisissez sa couleur : 
-        <select class="custom-select-lg">
-          <option selected="tan">Brun</option>
-          <option value="chocolate">Chocolat</option>
-          <option value="black">Noir</option>
-          <option value="white">Blanc</option>
-        </select>
-      </form>
-      <a href="#" class="btn btn-primary">Ajouter au panier</a>
+    newElt.innerHTML = `<img class="card-img-top" src="${teddy.imageUrl}" alt="Ourson ${teddy.name}">
+    <div class="card-body text-center">
+    <h5 class="card-title"><span class="name">${teddy.name}</span></h5>
+    <p class="description">${teddy.description}</p>
+    <div class="price">Prix : ${(teddy.price/100).toFixed(2) + " €"}</span></div>
+    <form>
+        <div class="custom">
+            <label for ="Choix de couleurs pour ${teddy.name}">Choisissez sa couleur :</label> 
+            <select class="custom-select"></select>
+            </div>
+    </form>
+    <a href="#" class="btn btn-primary">Ajouter au panier</a>
     </div>`;
     return newElt;  
 }
 
-fetchTeddies();
+// options de customisation
+function customTeddy(i) {
+    const newOpt = document.createElement("option");
+    newOpt.innerHTML = `${teddy.colors}`
+    return newOpt
+}
+    
+
+
+/* <option selected="tan">Brun</option>
+    <option value="chocolate">Chocolat</option>
+    <option value="black">Noir</option>
+    <option value="white">Blanc</option> */
+fetchTeddy();
